cbba565e2e074e82611c99e30cd6fa032d79cda8
Vedant <vedantdesai07@gmail.com>
2021-02-08 02:35:47 -0500

    tried clicking on the text using selenium -- n luck

--- night_audit_automation.py
+++ night_audit_automation.py
@@ -6,6 +6,8 @@ from dateutil.relativedelta import relativedelta
 import tabula
 from selenium.webdriver import ActionChains
 import hotel_journal_summary_report_analysis
+from selenium.webdriver.support.select import Select
+
 
 driver = webdriver.Chrome("chromedriver.exe")  # open google chrome using chrome driver
 
@@ -107,8 +109,7 @@ def night_audit_automation():
 
     root_directory = r'C:\Users\vedan\Downloads\{}'
 
-    hotel_journal_summary_report_file_name = (datetime.today() + relativedelta(days=-1)).strftime('%m_%d_%Y') + \
-        'hotel_journal_summary '
+    hotel_journal_summary_report_file_name = (datetime.today() + relativedelta(days=-1)).strftime('%m_%d_%Y') + 'hotel_journal_summary'
 
     tabula.convert_into(root_directory.format(hotel_journal_summary_report_file_name + '.pdf'),
                         root_directory.format(hotel_journal_summary_report_file_name + '.csv'), output_format="csv",
@@ -133,7 +134,65 @@ def night_audit_automation():
 
     time.sleep(5)
 
-    driver.find_elements_by_class_name("CHI_Button")
+    # driver.find_elements_by_class_name("CHI_Button")
+
+    driver.find_element_by_xpath('// *[(@ id = "act6")] / option[text() = "Night Audit"]').click()
+
+# night_audit_automation()
 
 
-night_audit_automation()
+print("I am here")
+time.sleep(1)
+
+driver.get(link_to_open)
+
+driver.execute_script("window.open()")
+
+driver.switch_to.window(driver.window_handles[1])
+
+main_window = driver.switch_to.window(driver.window_handles[0])
+
+print(main_window)
+
+time.sleep(0.5)
+
+e = driver.find_elements_by_class_name("CHI_Cell")
+
+print(e)
+time.sleep(1)
+
+a = driver.find_element_by_xpath("//input")
+# e.send_keys("vedantdesai07@gmail.com")
+
+print(a)
+
+a.send_keys("Vdesai.TXI54")
+
+pyautogui.hotkey('tab')
+
+pyautogui.write('Vbd@251196')
+
+pyautogui.hotkey('tab')
+
+pyautogui.hotkey('enter')
+# driver.find_element_by_xpath(
+#     '//*[contains(concat( " ", @class, " " ), concat( " ", "CHI_PageSection", " " ))]').click()
+
+time.sleep(3)
+
+driver.find_element_by_xpath('//*[(@ id = "act6")]').click()
+
+# driver.find_elements_by_id('menu7_1').click()
+
+# Select.select_by_visible_text('Night Audit')
+
+a = driver.find_elements_by_xpath('//*[text() = "Night Audit"]').click()
+
+print(a)
+
+a = ['<selenium.webdriver.remote.webelement.WebElement (session="b795e3a76eea02332b5b546865cbe69a", element="50d68991-fe7d-4b3f-95b8-a3da9c73d10f")>', '<selenium.webdriver.remote.webelement.WebElement (session="b795e3a76eea02332b5b546865cbe69a", element="dee98abd-c8d4-4166-8ab6-2d43fcf73e6d")>']
+
+print(a[0])
+# driver.find_elements_by_xpath('//*[text() = "Night Audit"]')[0].click()
+# select by visible text
+# select.select_by_visible_text('Banana')
