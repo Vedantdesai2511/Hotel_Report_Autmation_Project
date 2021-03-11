7a1563c019367a0c4dad62191a4211d6b01e8aa0
Vedant <vedantdesai07@gmail.com>
2021-03-11 00:55:20 -0500

    added all the udated files

---
+++ data_management_house_keeping_new.py
@@ -0,0 +1,137 @@
+import tabula
+import pandas as pd
+from datetime import datetime
+import pdfkit
+from print_on_the_specific_printer import print_the_file
+import re
+
+
+def house_keeping_report_function(report_1, report_2, room_list=None):
+    df1 = pd.read_csv(report_1)
+    # df2 = pd.read_csv(report_2)
+
+    print(df1.columns)
+    # print(df2)
+    df1.drop([0, 1], inplace=True)
+    df1.drop(df1.tail(1).index, inplace=True)
+
+    print(df1['Housekeeping Check-Off List'])
+    a = str(df1['Housekeeping Check-Off List'])
+    # print(str(a)[0])
+
+    data = {}
+
+    print(df1)
+
+    for i in ['Room', 'Status', 'Condition', 'Stay/C/O']:
+        data[i] = []
+
+    for idx, i in enumerate(df1['Housekeeping Check-Off List']):
+        print(idx)
+        # a = str(df1['Housekeeping Check-Off List'])
+        temp = re.findall(r'\d+', i)
+        res = list(map(int, temp))
+        data['Room'].append(res[-1])
+        data['Status'].append('VAC')
+        data['Condition'].append('Dirty')
+        data['Stay/C/O'].append('NAN')
+
+    print(data)
+
+    df1 = pd.read_csv(report_2)
+
+    df1.drop([0, 1], inplace=True)
+    df1.drop(df1.tail(1).index, inplace=True)
+
+    a = df1.drop(df1.loc[df1['Unnamed: 5'] == "Stay"].index)
+    print(a)
+
+    for idx, i in enumerate(a['Housekeeping Check-Off List']):
+        print(idx)
+        # a = str(df1['Housekeeping Check-Off List'])
+        temp = re.findall(r'\d+', i)
+        res = list(map(int, temp))
+        data['Room'].append(res[-1])
+        data['Status'].append('C/O')
+        data['Condition'].append('Dirty')
+        data['Stay/C/O'].append('C/O')
+
+    print(data)
+    df = pd.DataFrame(data, columns=['Room', 'Status', 'Condition', 'Stay/C/O'])
+    df.to_csv("final_output_.csv")
+
+    path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
+    csv_file = 'final_output_.csv'
+    html_file = csv_file[:-3] + 'html'
+
+    df = pd.read_csv('final_output_.csv', sep=',')
+    df.to_html(html_file)
+
+    file = open('final_output_.html', 'r')
+    a = file.read()
+
+    # file = codecs.open("final_output.html", "r", "utf-8")  # different method to open the html file
+    # a = file.read()
+
+    f = open('final_output_.html', 'w')
+
+    message = """<style>
+        th {
+          font-size: 30px;
+        }
+    
+        td {
+          font-size: 45px;
+        }
+        </style>
+    
+        """ + a
+
+    # print(f.read())
+
+    f.write(message)
+    f.close()
+
+    print("Printing combined House-keeping Report")
+
+    pdfkit.from_file(html_file, 'house_keeping_report_.pdf', configuration=config)
+
+    print_the_file('house_keeping_report_.pdf')
+
+# root_directory = r'C:\Users\vedan\Downloads\{}'
+#
+# name_of_the_file_HK_1_ = (datetime.today()).strftime('%m_%d_%Y') + 'house_keeping_list_1'
+# name_of_the_file_HK_2_ = (datetime.today()).strftime('%m_%d_%Y') + 'house_keeping_list_2'
+#
+# # tabula.convert_into(root_directory.format(name_of_the_file_HK_1_ + '.pdf'), root_directory.format(name_of_the_file_HK_1_ + '.csv'), output_format="csv", stream=True, pages=1)
+# # tabula.convert_into(root_directory.format(name_of_the_file_HK_2_ + '.pdf'), root_directory.format(name_of_the_file_HK_2_ + '.csv'), output_format="csv", stream=True, pages=1)
+#
+# house_keeping_report_function(root_directory.format(name_of_the_file_HK_1_ + '.csv'), root_directory.format(name_of_the_file_HK_2_ + '.csv'), room_list=None)
+#
+# path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+# config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+#
+# csv_file = 'final_output.csv'
+# html_file = csv_file[:-3]+'html'
+#
+# df = pd.read_csv('final_output.csv', sep=',')
+# df.to_html(html_file)
+#
+# # with open('header.html', 'r') as f:
+# #     html_string = f.read()
+#
+# options = {
+#     'page-size': 'Letter',
+#     'margin-top': '0.75in',
+#     'margin-right': '0.75in',
+#     'margin-bottom': '0.75in',
+#     'margin-left': '0.75in',
+#     'encoding': "UTF-8",
+#     'no-outline': None,
+#     'header-html': "C:/Users/vedan/PycharmProjects/upfib_algo_trading/header.html"
+# }
+#
+# pdfkit.from_file(html_file, 'house_keeping_report.pdf', configuration=config, options=options)
+# pdfkit.from_file(html_file, 'house_keeping_report.pdf', configuration=config)
--- report_automation.py
+++ report_automation.py
@@ -9,6 +9,7 @@ import schedule
 import os
 import tabula
 import data_management_house_keeping
+import data_management_house_keeping_new
 import pdfkit
 import pandas as pd
 from night_audit_automation import night_audit_automation
@@ -52,7 +53,7 @@ def report_automation():
 
         pyautogui.hotkey('tab')
 
-        pyautogui.write('Vbd@251196')
+        pyautogui.write('VBD@251196')
 
         pyautogui.hotkey('tab')
 
@@ -243,60 +244,66 @@ def report_automation():
 
         print("Report sent")
 
-    email_occupancy_snap_shot_reports_using_libraries(["sumit@dalwadi.com", "nathan.overton@dalwadi.com"])
-    email_occupancy_snap_shot_reports_using_libraries(["reportautomation1@gmail.com"])
+    # email_occupancy_snap_shot_reports_using_libraries(["sumit@dalwadi.com", "nathan.overton@dalwadi.com"])
+    # email_occupancy_snap_shot_reports_using_libraries(["reportautomation1@gmail.com"])
 
     name_of_the_file_HK_1_ = (datetime.today()).strftime('%m_%d_%Y') + 'house_keeping_list_1'
     name_of_the_file_HK_2_ = (datetime.today()).strftime('%m_%d_%Y') + 'house_keeping_list_2'
 
-    tabula.convert_into(root_directory.format(name_of_the_file_HK_1_ + '.pdf'),
-                        root_directory.format(name_of_the_file_HK_1_ + '.csv'), output_format="csv", stream=True,
-                        pages=1)
-    tabula.convert_into(root_directory.format(name_of_the_file_HK_2_ + '.pdf'),
-                        root_directory.format(name_of_the_file_HK_2_ + '.csv'), output_format="csv", stream=True,
-                        pages=1)
+    c = pdftables_api.Client('ewtqttj2079m')
+    c.csv(root_directory.format(name_of_the_file_HK_1_ + '.pdf'),
+          root_directory.format(name_of_the_file_HK_1_))
 
-    data_management_house_keeping.house_keeping_report_function(root_directory.format(name_of_the_file_HK_1_ + '.csv'),
+    c.csv(root_directory.format(name_of_the_file_HK_2_ + '.pdf'),
+          root_directory.format(name_of_the_file_HK_2_))
+    # tabula.convert_into(root_directory.format(name_of_the_file_HK_1_ + '.pdf'),
+    #                     root_directory.format(name_of_the_file_HK_1_ + '.csv'), output_format="csv", stream=True,
+    #                     pages=1)
+    # tabula.convert_into(root_directory.format(name_of_the_file_HK_2_ + '.pdf'),
+    #                     root_directory.format(name_of_the_file_HK_2_ + '.csv'), output_format="csv", stream=True,
+    #                     pages=1)
+
+    data_management_house_keeping_new.house_keeping_report_function(root_directory.format(name_of_the_file_HK_1_ + '.csv'),
                                                                root_directory.format(name_of_the_file_HK_2_ + '.csv'),
                                                                room_list=None)
 
-    path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
-    config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
-
-    csv_file = 'final_output.csv'
-    html_file = csv_file[:-3] + 'html'
-
-    df = pd.read_csv('final_output.csv', sep=',')
-    df.to_html(html_file)
-
-    file = open('final_output.html', 'r')
-    a = file.read()
-
-    # file = codecs.open("final_output.html", "r", "utf-8")  # different method to open the html file
+    # path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+    # config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+    #
+    # csv_file = 'final_output.csv'
+    # html_file = csv_file[:-3] + 'html'
+    #
+    # df = pd.read_csv('final_output.csv', sep=',')
+    # df.to_html(html_file)
+    #
+    # file = open('final_output.html', 'r')
     # a = file.read()
-
-    f = open('final_output.html', 'w')
-
-    message = """<style>
-        th {
-          font-size: 20px;
-        }
-
-        td {
-          font-size: 30px;
-        }
-        </style>
-
-        """ + a
-
-    # print(f.read())
-
-    f.write(message)
-    f.close()
-
-    print("Printing combined House-keeping Report")
-
-    pdfkit.from_file(html_file, 'house_keeping_report.pdf', configuration=config)
+    #
+    # # file = codecs.open("final_output.html", "r", "utf-8")  # different method to open the html file
+    # # a = file.read()
+    #
+    # f = open('final_output.html', 'w')
+    #
+    # message = """<style>
+    #     th {
+    #       font-size: 20px;
+    #     }
+    #
+    #     td {
+    #       font-size: 30px;
+    #     }
+    #     </style>
+    #
+    #     """ + a
+    #
+    # # print(f.read())
+    #
+    # f.write(message)
+    # f.close()
+    #
+    # print("Printing combined House-keeping Report")
+    #
+    # pdfkit.from_file(html_file, 'house_keeping_report.pdf', configuration=config)
 
     time.sleep(2)
 
@@ -331,10 +338,10 @@ def report_automation():
         os.remove(file)
 
 
-schedule.every().day.at("07:30").do(report_automation)
+# schedule.every().day.at("07:30").do(report_automation)
 # schedule.every().day.at("01:05").do(night_audit_automation)03_08_2021report
 # night_audit_automation()
-# report_automation()
+report_automation()
 
 while True:
     schedule.run_pending()
